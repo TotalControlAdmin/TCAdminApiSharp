fc73cd8d92e0fbd82b2f94c1abda5e4ddd1b81a1
Alex Redding <Alexredding180@gmail.com>
2021-01-23 00:41:41 +0000

    Implemented Server

---
+++ TCAdminApiSharp/Controllers/ServersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using RestSharp;
+using TCAdminApiSharp.Entities.API;
+using TCAdminApiSharp.Entities.Server;
+using TCAdminApiSharp.Entities.Service;
+using TCAdminApiSharp.Exceptions;
+using TCAdminApiSharp.Exceptions.API;
+
+namespace TCAdminApiSharp.Controllers
+{
+    public class ServersController : BaseController
+    {
+        public ServersController() : base("api/server")
+        {
+        }
+
+        // public int CreateService(ServiceBuilder builder)
+        // {
+        //     var body = builder.GenerateRequestBody();
+        //     var request = GenerateDefaultRequest();
+        //     request.Resource += "create";
+        //     request.Method = Method.POST;
+        //     request.AddParameter("createinfo", body, ParameterType.GetOrPost);
+        //     return ExecuteBaseResponseRequest<int>(request).Result;
+        // }
+        
+        public Server GetServer(int serverId)
+        {
+            try
+            {
+                var request = GenerateDefaultRequest();
+                request.Resource += serverId;
+                return ExecuteBaseResponseRequest<Server>(request).Result;
+            }
+            catch (ApiResponseException e)
+            {
+                if (e.ErrorResponse.RestResponse.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new NotFoundException(typeof(Service), e, new []{serverId});
+                }
+
+                throw;
+            }
+        }
+        
+        public ListResponse<Server> GetServices()
+        {
+            var request = GenerateDefaultRequest();
+            request.Resource += "servers";
+            return ExecuteListResponseRequest<Server>(request);
+        }
+    }
+}
